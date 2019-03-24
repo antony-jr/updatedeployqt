c565bba7c0c1975abc0c98fac0c0c3cbea5c3b60
antony-jr <antonyjr@protonmail.com>
2019-03-24 08:19:31 +0530

    add appdir_context class

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <qmake_process.h>
+#include <appdir_context.h>
 #include <logger.h>
 
 int main(int argc , char **argv){
@@ -34,14 +35,18 @@ int main(int argc , char **argv){
 		return 0;
 	}
 
-	putchar('\n');	
+	putchar('\n');
+
+	int ret = 0;	
 	char *appdir = NULL,
 	     *qmake_path = NULL;
 	qmake_process_t *qmakep = NULL;
+	appdir_context_t *appdir_ctx = NULL;
 
 	while(*(++argv)){
 		if(!strcmp(*argv , "-v") ||
 		   !strcmp(*argv , "--version")){
+			ret = -1;
 			goto cleanup;
 		}else 
 		if(!strcmp(*argv , "-a") ||
@@ -51,6 +56,7 @@ int main(int argc , char **argv){
 				continue;
 			}
 			printl(fatal , "expected appdir path");
+			ret = -2;
 			goto cleanup;
 		}else
 		if(!strcmp(*argv , "-q") ||
@@ -60,36 +66,56 @@ int main(int argc , char **argv){
 				continue;
 			}
 			printl(fatal , "expected qmake binary path");
+			ret = -3;
 			goto cleanup;
 		}else{ 
 			printl(fatal , "unknown or invalid argument '%s'" , *argv);
+			ret = -4;
 			goto cleanup;
 		}
 	}
 
 	if(!appdir){
 		printl(fatal , "no appdir path is given");
+		ret = -5;
+		goto cleanup;
+	}
+	
+	if(!(appdir_ctx = appdir_context_create(appdir))){
+		printl(fatal , "cannot create a appdir context");
+		ret = -6;
 		goto cleanup;
 	}
 
 	if(!(qmakep = qmake_process_create((!qmake_path) ? "qmake" : qmake_path))){
 		printl(fatal , "cannot create qmake process");
+		ret = -7;
 		goto cleanup;
 	}
 
 	const char *qt_version = qmake_query_result_value(
 					qmake_process_query(qmakep , "QT_VERSION")
 				 );
-
+	const char *qt_libs    = qmake_query_result_value(
+					qmake_process_query(qmakep , "QT_INSTALL_LIBS")
+				 );
+	const char *qt_plugins = qmake_query_result_value(
+				        qmake_process_query(qmakep , 
+						            "QT_INSTALL_PLUGINS")
+				 );
 	printl(info , "Qt version is %s" , qt_version);
+	printl(info , "Qt library install path %s" , qt_libs);
+	printl(info , "Qt plugins path %s" , qt_plugins);
 
 cleanup:
 	if(qmakep)
 		qmake_process_destroy(qmakep);
-	if(appdir)
+	if(appdir){
 		free(appdir);
+		appdir_context_destroy(appdir_ctx);
+	}
 	if(qmake_path)
 		free(qmake_path);
-	return 0;
+	return ret;
 }
 
